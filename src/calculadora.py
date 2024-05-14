4830a9551a34bec11450a4f892cea5de2a538161
Cristian De Martino <A23216380@alumnos.uady.mx>
2024-05-13 20:27:33 -0600

    nueva division

--- src/calculadora.py
+++ src/calculadora.py
@@ -16,28 +16,3 @@ def multiplicar_num(num1, num2):
 def dividir_num(num1, num2):
     num1 / num2
     return num1 / num2
-
-
-def calcular(num1, num2, operador):
-    resultado = None
-    messaggio = "Input non valido"
-    if operador == "+":
-        resultado = sumar_num(num1, num2)
-        messaggio = f"El resultado de la suma es: {resultado:.2f}"
-    elif operador == "-":
-        resultado = restar_num(num1, num2)
-
-    elif operador == "*":
-        resultado = multiplicar_num(num1, num2)
-
-    elif operador == "/":
-
-        if num2 != 0:
-            resultado = dividir_num(num1, num2)
-        else:
-            # throw error division by zero
-            print("Error: división por cero.")
-    else:
-        # throw error operador non valido
-        print("Operador no válido. Intente de nuevo.")
-    return [resultado, messaggio]
